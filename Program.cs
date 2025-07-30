2a402c0fea6d8ebff4fc73834b80cf515d7bace2
MANAHIL ABID <soniaabid806@gmail.com>
2025-07-30 15:52:46 +0500

    Escape Sequence

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("this is my first c# file");
-Console.WriteLine("Enter Your name");
-Console.ReadLine();
-Console.WriteLine("Enter Your Email");
-Console.ReadLine();
+Console.WriteLine("second class");
+Console.WriteLine("ESCAPE SEQUENCE");
+//for single codes
+Console.WriteLine("---------single codes-----------");
+Console.WriteLine("\'hello world\'");
+//for double codes
+Console.WriteLine("---------double codes----------");
+Console.WriteLine("\"hello world\"");
+//for line break
+Console.WriteLine("-----------line break------------");
+Console.Write("\nhello world\n");
 Console.ReadKey();
